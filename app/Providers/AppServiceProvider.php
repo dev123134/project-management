2dc01b3312cec437639cf459dcac1b886a95d70e
DEV <devbhagat069@gmail.com>
2026-07-24 09:20:44 +0530

    force https scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\Notification;
 use Illuminate\Support\Facades\Gate;
 use App\Models\User;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
 public function boot(): void
 {
+    if (config('app.env') === 'production') {
+    URL::forceScheme('https');
+}
+
     Gate::define('admin', function (User $user) {
         return $user->role === 'admin';
     });
